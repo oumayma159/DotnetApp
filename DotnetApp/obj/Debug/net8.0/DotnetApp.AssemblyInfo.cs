2dccd77de2d037271017506d36e4e841f655bbee
oumayma159 <oumaima.elfidha@insat.ucar.tn>
2023-12-04 01:08:24 +0100

    first

--- DotnetApp/obj/Debug/net8.0/DotnetApp.AssemblyInfo.cs
+++ DotnetApp/obj/Debug/net8.0/DotnetApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc2451648710c990d6837c83e5a2c108468a3192")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
